54cd40394497822de80afad70e325ce1dec68ace
Cynthia Caron <cynthiacaron@macbook-pro-de-cynthia.home>
2024-03-22 16:16:06 +0100

    update images

--- src/data/images.js
+++ src/data/images.js
@@ -1,43 +1,52 @@
 const images = [
   {
-    image: 'images/1.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/1.jpg'
   },
   {
-    image: 'images/2.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/2.jpg'
   },
   {
-    image: 'images/3.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/3.jpg'
   },
   {
-    image: 'images/4.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/4.jpg'
   },
   {
-    image: 'images/5.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/5.jpg'
   },
   {
-    image: 'images/6.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/6.jpg'
   },
   {
-    image: 'images/7.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/7.jpg'
   },
   {
-    image: 'images/8.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/8.jpg'
   },
   {
-    image: 'images/9.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/9.jpg'
   },
   {
-    image: 'images/10.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/10.jpg'
   },
   {
-    image: 'images/11.jpg'
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/11.jpg'
   },
   {
-    image: 'images/12.jpg'
-  },
-  {
-    image: 'images/13.jpg'
-  },
+    image:
+      'https://raw.githubusercontent.com/supahfunk/webgl-carousel/main/public/img/12.jpg'
+  }
 ]
 
 export default images
